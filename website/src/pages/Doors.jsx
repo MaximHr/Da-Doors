414a5061cb26921b5c69466b4deac0baf5be2712
Maxim <hristivi8383@gmail.com>
2026-03-17 18:50:02 +0200

    pagination problem

--- website/src/pages/Doors.jsx
+++ website/src/pages/Doors.jsx
@@ -23,13 +23,19 @@ const Doors = ({ series }) => {
     setDoors(data.content);
     setTotalElements(data.totalElements);
     setTotalPages(data.totalPages);
-		setPage(0);
   };
 
   useEffect(() => {
     loadProducts(page);
+		setPage(0);
     window.scrollTo({ top: 0, behavior: "smooth" });
-  }, [page, series, name]);
+  }, [series, name]);
+
+	  useEffect(() => {
+    loadProducts(page);
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }, [page]);
+
 
   const handlePrev = () => {
     if (page > 0) setPage((prev) => prev - 1);
